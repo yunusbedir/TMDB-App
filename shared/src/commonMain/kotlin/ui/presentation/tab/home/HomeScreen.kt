87fa9fa0006ee71b7210834698f2ac8eb40875ce
yunusbedir <yunus.bedir@btc-ag.com.tr>
2023-08-22 17:22:53 +0300

    changed design HomeScreen

--- shared/src/commonMain/kotlin/ui/presentation/tab/home/HomeScreen.kt
+++ shared/src/commonMain/kotlin/ui/presentation/tab/home/HomeScreen.kt
@@ -1,46 +1,19 @@
 package ui.presentation.tab.home
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Tab
+import androidx.compose.material.TabRow
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import cafe.adriel.voyager.core.screen.Screen
-import cafe.adriel.voyager.navigator.LocalNavigator
-import cafe.adriel.voyager.navigator.Navigator
-import cafe.adriel.voyager.navigator.bottomSheet.BottomSheetNavigator
-import cafe.adriel.voyager.navigator.bottomSheet.LocalBottomSheetNavigator
-import cafe.adriel.voyager.navigator.currentOrThrow
-import domain.model.result.BaseMediaModel
-import ui.components.AutoScrollingHorizontalSlider
-import ui.components.CapsuleButton
-import ui.components.NumberMediaCard
-import ui.components.TopMediaCard
+import ui.presentation.movie.MovieScreen
+import ui.presentation.tv.TvScreen
 
 
 internal class HomeScreen(
@@ -49,131 +22,24 @@ internal class HomeScreen(
 
     @Composable
     override fun Content() {
-        HomeScreenComponent(
-            viewModel = viewModel
-        )
-    }
-}
-
-@Composable
-fun HomeScreenComponent(
-    viewModel: HomeScreenViewModel,
-    bottomSheetNavigator: BottomSheetNavigator = LocalBottomSheetNavigator.current,
-    navigator: Navigator = LocalNavigator.currentOrThrow
-) {
-
-    LaunchedEffect("homeScreenViewModel") {
-        println("MY_LOG , homeScreenViewModel.getMovies()")
-        viewModel.getTrending()
-        viewModel.getMovies()
-    }
-
-    val uiState = viewModel.uiState.collectAsState()
-    Column(
-        Modifier.fillMaxWidth(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.TopStart) {
-            MediaList(
-                trendsOfDay = uiState.value.trendsOfDay,
-                popularMovies = uiState.value.popularMovies,
-                modifier = Modifier.fillMaxWidth()
-            )
-        }
-
-    }
-}
-
-@Composable
-fun MediaList(
-    trendsOfDay: List<BaseMediaModel>,
-    popularMovies: List<BaseMediaModel>,
-    modifier: Modifier = Modifier
-) {
-    LazyColumn(
-        modifier = modifier,
-        state = rememberLazyListState(),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        item {
-            AnimatedVisibility(visible = trendsOfDay.isNotEmpty()) {
-                AutoScrollingHorizontalSlider(trendsOfDay.size) { page ->
-                    val media = trendsOfDay[page]
-                    TopMediaCard(
-                        media = media,
-                        onPlayClick = { },
-                        onDetailsClick = { }
+        var tabIndex by remember { mutableStateOf(0) }
+        val tabs = listOf("Movie", "Tv")
+        Column(modifier = Modifier.fillMaxWidth()) {
+            TabRow(selectedTabIndex = tabIndex) {
+                tabs.forEachIndexed { index, title ->
+                    Tab(text = { Text(title) },
+                        selected = tabIndex == index,
+                        onClick = { tabIndex = index }
                     )
                 }
             }
-        }
-        item {
-            TitleAndMediaLazyRow(
-                title = "Popular Movie",
-                mediaList = popularMovies,
-                modifier = Modifier.padding(horizontal = 8.dp)
-            )
-        }
-        item {
-            TitleAndMediaLazyRow(
-                title = "Popular Tv",
-                mediaList = popularMovies,
-                modifier = Modifier.padding(horizontal = 8.dp)
-            )
-        }
-        item {
-            TitleAndMediaLazyRow(
-                title = "Discover Movie",
-                mediaList = popularMovies,
-                modifier = Modifier.padding(horizontal = 8.dp)
-            )
-        }
-        item {
-            TitleAndMediaLazyRow(
-                title = "Discover Tv",
-                mediaList = popularMovies,
-                modifier = Modifier.padding(horizontal = 8.dp)
-            )
-        }
-    }
-}
+            when (tabIndex) {
+                0 -> MovieScreen(
+                    viewModel = viewModel
+                )
 
-@Composable
-fun TitleAndMediaLazyRow(
-    title: String,
-    mediaList: List<BaseMediaModel>,
-    modifier: Modifier = Modifier
-) {
-    Column(modifier = modifier.fillMaxWidth()) {
-        Row(
-            modifier = Modifier.fillMaxWidth()
-                .padding(bottom = 8.dp, top = 4.dp),
-            verticalAlignment = Alignment.Bottom,
-            horizontalArrangement = Arrangement.SpaceAround
-        ) {
-            Text(
-                text = title,
-                fontWeight = FontWeight.Bold,
-                textAlign = TextAlign.Start,
-                modifier = Modifier.fillMaxWidth().weight(1f)
-                    .wrapContentSize(Alignment.Center)
-            )
-            Spacer(modifier = Modifier.fillMaxWidth().weight(1f))
-            Text(
-                modifier = Modifier.fillMaxWidth().weight(1f),
-                text = "Show More..",
-                textAlign = TextAlign.Center,
-                color = Color.LightGray,
-                fontSize = 12.sp
-            )
-        }
-
-        LazyRow {
-            itemsIndexed(mediaList) { index, item ->
-                NumberMediaCard(
-                    index = index,
-                    media = item,
-                    modifier = Modifier
+                1 -> TvScreen(
+                    viewModel = viewModel
                 )
             }
         }
